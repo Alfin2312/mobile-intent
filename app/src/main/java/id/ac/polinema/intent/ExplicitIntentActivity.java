419067c1e8d7729bb192e50425da5beab498a70d
Alfin Mulya Adi Pratama <alfin.mulya1999@gmail.com>
2020-02-20 16:21:37 +0700

    Praktikum Intent

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,6 +1,9 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +15,9 @@ public class ExplicitIntentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_explicit_intent);
     }
 
+    public void tombolOnClick(View arg0) {
+        EditText edit = findViewById(R.id.input_name);
+        TextView text = findViewById(R.id.text_output);
+        text.setText(edit.getText());
+    }
 }
